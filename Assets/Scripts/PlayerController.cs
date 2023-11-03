671e24ab4e0c021d705dab2ae4f192393b8f112f
eex9 <eex9.gs@gmail.com>
2023-11-03 11:02:29 -0400

    the most bare bones player controller i could make
    i dont wanna code rn

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move() 
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.Translate(Vector3.right * horizontal);
+        transform.Translate(Vector3.up * vertical);
+    }
+}
