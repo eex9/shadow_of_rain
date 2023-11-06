b00931f41055604e5a6cc3f44e867278a9cf68c1
eex9 <eex9.gs@gmail.com>
2023-11-06 10:31:46 -0500

    update the character controller
    we can jump now! also i broke collisions but thats fine

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float posX;
+    public float posY;
+    public float posZ;
+
+    public float jumpHeight;
+    readonly float JUMPSPEED = 60; // 60 = 1 second
+    bool isJumping;
+    float jumpTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +31,29 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
-        transform.Translate(Vector3.right * horizontal);
-        transform.Translate(Vector3.up * vertical);
+        posX += horizontal;
+        posZ += vertical;
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            isJumping = true;
+            jumpTimer = JUMPSPEED * 2;
+        }
+
+        if (isJumping) 
+        {
+            posY += (1/jumpHeight) * Time.deltaTime * (jumpTimer-JUMPSPEED)/2; // math .
+            jumpTimer--;
+        }
+
+        if (jumpTimer <= 0) {
+            isJumping = false;
+            posY = 0;
+        }
+
+        transform.position = new Vector2(posX, posY + posZ); // set the player's position
+
+        GameObject shadow = GameObject.Find("Shadow"); // find the shadow and move it to below the player
+        shadow.transform.position = new Vector2(posX, posZ - 0.5f);
     }
 }
