fd1836cefc62d276fbee95cf7cef3b2f2534579a
eex9 <eex9.gs@gmail.com>
2024-04-16 12:16:24 -0400

    Change Player Control method yet again

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,56 +4,69 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    public float posX;
-    public float posY;
-    public float posZ;
+    public Rigidbody2D rb;
+    public Collider2D cl;
 
+    public float speed;
+
+    bool isJumping = false;
+    float jumpTimer = 0;
+    float addY = 0;
     public float jumpHeight;
     readonly float JUMPSPEED = 60; // 60 = 1 second
-    bool isJumping;
-    float jumpTimer;
+    readonly float FORCE_STRENGTH = 100;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        HandleMovement();
     }
 
-    void Move() 
+    void HandleMovement()
     {
-        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        // Multiply by the constant FORCE_STRENGTH bc unity forces are dumb.
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime * FORCE_STRENGTH;
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime * FORCE_STRENGTH;
 
-        posX += horizontal;
-        posZ += vertical;
-
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
         {
-            isJumping = true;
             jumpTimer = JUMPSPEED * 2;
+            isJumping = true;
         }
 
-        if (isJumping) 
+        if (isJumping)
         {
-            posY += (1/jumpHeight) * Time.deltaTime * (jumpTimer-JUMPSPEED)/2; // math .
+            // Create a graph that accellerates upwards in reducing amounts in the first half, then accellerates downwards.
+            addY = -(JUMPSPEED - jumpTimer) * Time.deltaTime * 1/jumpHeight;
             jumpTimer--;
+            if (jumpTimer <= -1)
+            {
+                addY = 0;
+                isJumping = false;
+                jumpTimer = 0;
+                cl.offset = new Vector2(0, 0);
+            }
         }
 
-        if (jumpTimer <= 0) {
-            isJumping = false;
-            posY = 0;
-        }
+        // Apply movement using unity forces system
+        rb.AddForce(new Vector2(horizontal, vertical));
+        cl.offset += new Vector2(0, addY);
+        transform.position += new Vector3(0, addY, 0);
+    }
 
-        transform.position = new Vector2(posX, posY + posZ); // set the player's position
+    void OnCollisionEnter2D(Collision2D other)
+    {
 
-        GameObject shadow = GameObject.Find("Shadow"); // find the shadow and move it to below the player
-        shadow.transform.position = new Vector2(posX, posZ - 0.5f);
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+
     }
 }
