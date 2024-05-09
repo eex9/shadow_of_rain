48f389e6a5bab4bcc18076a2f271157067bfbeaa
eex9 <eex9.gs@gmail.com>
2024-05-09 12:20:45 -0400

    fix bugs

--- Assets/Scripts/AbstractEnemy.cs
+++ Assets/Scripts/AbstractEnemy.cs
@@ -8,13 +8,15 @@ public class AbstractEnemy : MonoBehaviour
     public int sightTimer;
     public float sightRange;
     protected static readonly int layerMask = 3;
+    protected HeadsUpDisplay UIHandler;
 
+    void Start() {
+        UIHandler = (HeadsUpDisplay) GameObject.Find("HUD").GetComponent<HeadsUpDisplay>();
+    }
     void Update()
     {
-        // if (Input.GetKeyDown("return"))
-        // {
-            CastSight();
-        // }
+        
+        CastSight();
     }
 
     void StartSightTimer() { }
@@ -22,16 +24,15 @@ public class AbstractEnemy : MonoBehaviour
     void CastSight()
     {
         RaycastHit2D resultHit = Physics2D.CircleCast(this.transform.position, sightRange, Vector2.zero, layerMask);
-        Rigidbody2D resultRB = resultRB.GetComponent<Rigidbody>();
-        if (resultHit.gameObject.getComponent<PlayerController>() == null) return; 
+        Rigidbody2D resultRB = resultHit.rigidbody;
+        if (resultRB.gameObject.GetComponent<PlayerController>() == null) return; 
         Vector3 offset = resultRB.transform.position - this.transform.position;
         float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
         
         if (angle < -45) angle = 360 + angle;
         // Debug.Log(((int)(angle + 45))/90);
         if (((int)(angle + 45))/90 == (int)this.direction) {
-            (PlayerController) player = resultRB.gameObject.getComponent<PlayerController>();
-            player.Kill();
+            PlayerController player = (PlayerController) resultRB.gameObject.GetComponent<PlayerController>();
         }
     }
 }
