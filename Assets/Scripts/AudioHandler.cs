cf5435ba28bd74cb43be19bdda760d51bfcd510e
eex9 <eex9.gs@gmail.com>
2024-05-17 12:09:45 -0400

    add audiohandler

---
+++ Assets/Scripts/AudioHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioHandler : MonoBehaviour
+{
+    AudioSource bgm;
+    
+    void Start() {
+        bgm = this.GetComponent<AudioSource>();
+    }
+
+    void Update() {
+
+    }
+
+    void ChangeBGM(AudioClip music) {
+        if (bgm.clip.name == music.name) {
+            return;
+        }
+
+        bgm.Stop();
+        bgm.clip = music;
+        bgm.Play();
+    }
+}
